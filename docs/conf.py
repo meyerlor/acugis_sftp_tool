07d01d77589bd552df5c1ecd0e91ee1e0256faa7
David Ghedini <david@davidghedini.com>
2025-04-28 00:44:32 +0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -42,9 +42,9 @@ html_static_path = ['_static']
 
 # These paths are either relative to html_static_path
 # or fully qualified paths (eg. https://...)
-html_css_files = [
-    'css/custom.css',
-]
+# html_css_files = [
+#    'css/custom.css',
+# ]
 
 pygments_style = "sphinx"
 
